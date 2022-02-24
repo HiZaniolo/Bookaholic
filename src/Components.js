afa9d3bef582400d9f4891c400a9d02ab5e6293b
HiZaniolo <hizaniolo@gmail.com>
2022-02-24 11:32:21 +0000

    Componentes Nav Main y Footer agregados

---
+++ src/Components.js
@@ -0,0 +1,23 @@
+// const App = () => "Hola Mundo con app import"
+
+// export default App
+
+// import React from 'react' 
+
+import NavBar from "./NavBar"
+import Main from "./Main"
+import Footer from "./Footer"
+
+
+
+const Components = () => {
+    return (
+    <>
+        <NavBar />
+        <Main />
+        <Footer />
+    </>
+    )
+}
+
+export default Components
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>&copy; Copyright 2022 - Hilen Zaniolo</p>
+        <p>Todos los derechos reservados</p>
+    </footer>
+  )
+}
+
+export default Footer
